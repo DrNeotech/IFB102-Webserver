4839542fd27ea0da1340f2970601de7f2517da1a
DrNeotech <kaitlyn.oc.newton@gmail.com>
2025-05-23 03:09:38 +1000

    did stuff

--- frontend.py
+++ frontend.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('webpage.html')
 
 @app.route('/', methods=['POST'])
 def render_collage():
